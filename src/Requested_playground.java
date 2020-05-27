5235850cab878a186972953e38d35101669c656a
ramez morkos <ramezmorcos20150110@gmail.com>
2020-05-27 23:15:50 +0200

    continue implementation

---
+++ src/Requested_playground.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class Requested_playground {
+    ArrayList<playground> req_pl_gd;
+}
--- src/eWallet.java
+++ src/eWallet.java
@@ -1,2 +1,16 @@
 public class eWallet {
+   private double balance;
+   eWallet(){
+       balance=0;
+   }
+    eWallet(double b){
+        this.balance=b;
+    }
+    public void add_to_balance(double added){
+        balance+=added;
+    }
+    public double view_balance(){
+        return balance;
+    }
+
 }
